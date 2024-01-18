989ec6ca91df0fb49d77b4da6c71f9d34a74ed3e
Ydams <Damola4542@gmail.com>
2024-01-18 03:30:53 -0800

    0x19 C- stack,Queues-LIFO, FIFO ALX PROJECT COMPLETED

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+  *f_nop- nothing
+  *@head: the stack head
+  *@counter: d line_number
+  *Return: nothing
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_stack - The f_stack prints the top
+ * @head: the stack head
+ * @counter: d line_number
+ * Return: it brings no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
